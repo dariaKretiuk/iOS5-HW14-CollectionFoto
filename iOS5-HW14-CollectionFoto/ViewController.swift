80d441daeb256b0995671ba83604bb2628b1040f
dariaKretiuk <kretyukdasha@gmail.com>
2022-03-26 12:01:37 +0700

    Add project

---
+++ iOS5-HW14-CollectionFoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS5-HW14-CollectionFoto
+//
+//  Created by Дарья Кретюк on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
